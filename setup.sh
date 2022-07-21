d2b00271c98d67fc1cf881068719fca3db8dee99
tyropro <78479723+tyropro@users.noreply.github.com>
2022-07-21 15:56:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 echo "[-] touch .hushlogin"
 touch .hushlogin &>/dev/null && echo "[+] done" || echo "[!] errors"
+echo
 
 echo "[-] sudo apt update -y"
 sudo apt update -y &>/dev/null && echo "[+] done" || echo "[!] errors"
+echo
 
 echo "[-] sudo apt full-upgrade -y"
 sudo apt full-upgrade -y &>/dev/null && echo "[+] done" || echo "[!] errors"
+echo
 
 echo "[-] sudo reboot"
 sudo reboot
